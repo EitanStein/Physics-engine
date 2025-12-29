f6ab0b39f7c51deee36306c1100995281cf81cda
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2025-12-29 22:05:26 +0000

    add dot product function for points

--- include/PhysicsEngine/core/point.hpp
+++ include/PhysicsEngine/core/point.hpp
@@ -27,3 +27,7 @@ typedef Point DirVector;
 double dist(const Point& p1, const Point& p2){ 
     return std::sqrt(std::pow(p1.x  - p2.x, 2) + std::pow(p1.y  - p2.y, 2));
 }
+
+double dotProduct(const Point& p1, const Point& p2){
+    return p1.x * p2.x + p1.y * p2.y;
+}
