4078c6ef41540c7afd056bd8646a7ca4004764c4
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2025-12-29 21:44:45 +0000

    change point arithmetic operations to const methods

--- include/PhysicsEngine/core/point.hpp
+++ include/PhysicsEngine/core/point.hpp
@@ -13,10 +13,10 @@ struct Point{
     Point& operator=(const Point&) = default;
     Point& operator=(Point&&) = default;
 
-    Point operator*(double num) {return Point(x*num, y*num);}
-    Point operator/(double num) {return Point(x/num, y/num);}
-    Point operator+(const Point& other) {return Point(x+other.x, y+other.y);}
-    Point operator-(const Point& other) {return Point(x-other.x, y-other.y);}
+    Point operator*(double num) const {return Point(x*num, y*num);}
+    Point operator/(double num) const {return Point(x/num, y/num);}
+    Point operator+(const Point& other) const {return Point(x+other.x, y+other.y);}
+    Point operator-(const Point& other) const {return Point(x-other.x, y-other.y);}
     void operator+=(const Point& other) {x+=other.x; y+=other.y;}
     void operator*=(double num) {x*=num; y*=num;}
     bool operator==(const Point& other) const {return (x == other.x && y == other.y);}
