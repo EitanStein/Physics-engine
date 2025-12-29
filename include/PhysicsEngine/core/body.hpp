35f48670ef114310bf4bd5eda5c8896150bf8929
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2025-12-29 21:36:56 +0000

    add basic mass and restitution to bodies
    needed for impulse collision response

--- include/PhysicsEngine/core/body.hpp
+++ include/PhysicsEngine/core/body.hpp
@@ -8,8 +8,15 @@ private:
     Point position;
     DirVector velocity;
     DirVector acceleration;
+
+    double mass;
+    double restitution;
 public:
-    Body(Point pos, DirVector starting_v, DirVector starting_a) : position(pos), velocity(starting_v), acceleration(starting_a) {}
+    Body(Point pos, DirVector starting_v, DirVector starting_a) : position(pos), velocity(starting_v), acceleration(starting_a) { 
+        // TODO give non-const values
+        mass = 1;
+        restitution = 0.8;
+    }
     void update(double time_step) {
         // TODO reduce object creations here
         DirVector vel_increase = acceleration * time_step;
@@ -24,4 +31,6 @@ public:
     const Point& getPosition() const {return position;}
     const DirVector& getVelocity() const {return velocity;}
     const DirVector& getAcceleration() const {return acceleration;}
+    double getMass() const {return mass;}
+    double getRestitution() const {return restitution;}
 };
