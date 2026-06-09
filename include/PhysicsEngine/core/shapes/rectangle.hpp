de6e3d923f2c29ea6190a0656a2379d37cb04edf
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-06-09 14:48:01 +0000

    add half wifth/height getters to rectangle

--- include/PhysicsEngine/core/shapes/rectangle.hpp
+++ include/PhysicsEngine/core/shapes/rectangle.hpp
@@ -9,22 +9,23 @@ private:
     double width;
     double height;
 
-    // TODO change all /2 with these
-    // double half_width;
-    // double half_height;
+    double half_width;
+    double half_height;
 public:
-    Rectangle(double width, double height) : width(width), height(height) {}
+    Rectangle(double width, double height) : width(width), height(height),
+                                              half_width(width/2), half_height(height/2){}
     double getWidth() const {return width;}
     double getHeight() const {return height;}
+
+    double getHalfWidth() const {return half_width;};
+    double getHalfHeight() const {return half_height;}
+
     bool contains(const Point& pos, const Point& point) const override { 
         return (std::abs(pos.x - point.x) <= width/2) && (std::abs(pos.y - point.y) <= height/2);
     }
     ShapeT::Type type() const override {return ShapeT::Type::Rectangle; }
 
     std::array<Point,4> getVertices(const Point& pos, double angle) const{
-        double half_width = width / 2.0;
-        double half_height = height / 2.0;
-
         std::array<Point,4> vertices = {
             Point(-half_width, -half_height),
             Point(half_width, -half_height),
